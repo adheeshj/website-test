9245c160efe631bab808f114a54d88b0a88686c9
adheesh joshi <ch24btech11003@iith.ac.in>
2026-06-05 17:10:23 +0530

    Publish application

--- components/enquiry-modal.tsx
+++ components/enquiry-modal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useCallback } from 'react'
-import { X, Send, Loader2 } from 'lucide-react'
+import { X, Send, Loader as Loader2 } from 'lucide-react'
 import type { Product } from '@/data/products'
 
 type FormState = {
